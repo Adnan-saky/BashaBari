fc76a4f1a6db6451cb09621bbb53edc7e65ab988
msi <julfikar.123456@gmail.com>
2019-08-27 00:43:25 +0600

    ..

--- app/src/main/java/com/example/bashabari/_9Bills.java
+++ app/src/main/java/com/example/bashabari/_9Bills.java
@@ -2,13 +2,31 @@ package com.example.bashabari;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class _9Bills extends AppCompatActivity {
+    ////creating a object to hold the id of arrow button of layout 9
+    private ImageView arrow_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__09_bills);
+
+        ////getting the id of arrow button of layout 9
+        arrow_btn =  findViewById(R.id.arrow_btn_9);
+        arrow_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent9 = new Intent(_9Bills.this, _5UserHome.class);
+                startActivity(intent9);
+            }
+        });
+
+
     }
 }

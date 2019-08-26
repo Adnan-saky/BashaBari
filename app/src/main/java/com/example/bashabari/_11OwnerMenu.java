1f425e343cebca65bb931a1415d6fd66cda93041
msi <julfikar.123456@gmail.com>
2019-08-27 01:46:51 +0600

    321

--- app/src/main/java/com/example/bashabari/_11OwnerMenu.java
+++ app/src/main/java/com/example/bashabari/_11OwnerMenu.java
@@ -3,12 +3,43 @@ package com.example.bashabari;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class _11OwnerMenu extends AppCompatActivity {
+    ////creating a object to hold the id of the contents of layout 11
+    LinearLayout main_menu_layout;
+    RelativeLayout home_layout;
+    private ImageView menu_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__11_owner_menu);
+
+        ////getting the id of the contents of layout 11
+        main_menu_layout = findViewById(R.id.main_menu_layout_11);
+        home_layout = findViewById(R.id.home_layout_11);
+        menu_btn = findViewById(R.id.menu_btn_11);
+
+        menu_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                main_menu_layout.animate().translationX(0);
+                home_layout.animate().translationX(0);
+            }
+        });
+
+        home_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                main_menu_layout.animate().translationX(-975);
+                home_layout.animate().translationX(-975);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/bashabari/_6UserMenu.java
+++ app/src/main/java/com/example/bashabari/_6UserMenu.java
@@ -3,12 +3,41 @@ package com.example.bashabari;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class _6UserMenu extends AppCompatActivity {
+    ////creating a object to hold the id of the contents of layout 6
+    LinearLayout main_menu_layout;
+    RelativeLayout home_layout;
+    private ImageView menu_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__06_user_menu);
+
+        ////getting the id of the contents of layout 11
+        main_menu_layout = findViewById(R.id.main_menu_layout_6);
+        home_layout = findViewById(R.id.home_layout_6);
+        menu_btn = findViewById(R.id.menu_btn_6);
+
+        menu_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                main_menu_layout.animate().translationX(0);
+                home_layout.animate().translationX(0);
+            }
+        });
+
+        home_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                main_menu_layout.animate().translationX(-975);
+                home_layout.animate().translationX(-975);
+            }
+        });
     }
 }

18155e9efd931f649dc93ae0fc992edb1c7926d9
julfikar123456 <julfikar.123456@gmail.com>
2019-09-23 00:58:58 +0600

    reg

--- app/src/main/java/com/example/bashabari/_13RegisterTenants.java
+++ app/src/main/java/com/example/bashabari/_13RegisterTenants.java
@@ -1,5 +1,6 @@
 package com.example.bashabari;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,9 +8,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class _13RegisterTenants extends AppCompatActivity {
     ////creating a object to hold the id of arrow button of layout 13
@@ -44,34 +49,76 @@ public class _13RegisterTenants extends AppCompatActivity {
                 startActivity(intent12);
             }
         });
+
         reg_btn =  findViewById(R.id.reg_btn);
+
         reg_btn.setOnClickListener(new View.OnClickListener() {
+            //////////////////////////////////////////////////////////////////////////////////////////////
+            //////////////////////////////////////////////////////////////////////////////////////////////
             //Tick Button
             @Override
             public void onClick(View view) {
-                Intent intent1 = new Intent(_13RegisterTenants.this, _11OwnerMenu.class);
-                startActivity(intent1);
+                String name = Name.getText().toString().trim();
+                String address = Address.getText().toString().trim();
+                String nid_no = Nid_no.getText().toString().trim();
+                String phone_no = Phone_no.getText().toString().trim();
+                String password = Password.getText().toString().trim();
 
-                saveToDatabase();
+                ////////////////////////////////////////////////////////////////////////////////////////
+                /*databaseReference.child(phone_no).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        userInfo usrinf = dataSnapshot.getValue(userInfo.class);
+                        if(phone_no.equals(usrinf.getPhone_no())){
+                            Toast.makeText(getApplicationContext(),"Phone Already number exists",Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });*/
+
+                //if fields are not empty:
+                if( !name.isEmpty() && !address.isEmpty() && !nid_no.isEmpty() && !phone_no.isEmpty() && !password.isEmpty() ) {
+                    Intent intent1 = new Intent(_13RegisterTenants.this, _11OwnerMenu.class);
+                    startActivity(intent1);
+
+                    saveToDatabase(name, address, nid_no, phone_no, password);
+                }
+
+                //else fields are empty
+                else{
+                    Toast.makeText(getApplicationContext(),"Empty Input Field",Toast.LENGTH_SHORT).show();
+
+                    if(name.isEmpty())
+                        Name.setError("Input your name.");
+                    else if(address.isEmpty())
+                        Address.setError("Input your address");
+                    else if(nid_no.isEmpty())
+                        Nid_no.setError("Input your National ID Number");
+                    else if(password.isEmpty())
+                        Password.setError("Input a password");
+                    else if(phone_no.isEmpty())
+                        Phone_no.setError("Input your address");
+                }
+
+
+
             }
         });
     }
 
-    public void saveToDatabase(){
+    public void saveToDatabase(String name, String address, String nid_no, String phone_no, String password){
         //it will save registration data in database
-        String name = Name.getText().toString().trim();
-        String address = Address.getText().toString().trim();
-        String nid_no = Nid_no.getText().toString().trim();
-        String phone_no = Phone_no.getText().toString().trim();
-        String password = Password.getText().toString().trim();
-        if( !name.isEmpty() && !address.isEmpty() && !nid_no.isEmpty() && !phone_no.isEmpty() && !password.isEmpty() ) {
-            userInfo tenants = new userInfo(name, address, nid_no, phone_no, password);
+        userInfo tenants = new userInfo(name, address, nid_no, phone_no, password);
+        String key = phone_no;
 
-            String key = phone_no;
+        databaseReference.child(key).setValue(tenants);
 
-
-            databaseReference.child(key).setValue(tenants);
-        }
-
+        //toast for showing registration done message
+        Toast.makeText(getApplicationContext(),"Registration Done",Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/example/bashabari/_4Register.java
+++ app/src/main/java/com/example/bashabari/_4Register.java
@@ -1,5 +1,6 @@
 package com.example.bashabari;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,8 +11,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class _4Register extends AppCompatActivity {
     ///////////////////////variable declaration
@@ -45,8 +49,25 @@ public class _4Register extends AppCompatActivity {
                 String name = Name.getText().toString().trim();
                 String address = Address.getText().toString().trim();
                 String nid_no = Nid_no.getText().toString().trim();
-                String phone_no = Phone_no.getText().toString().trim();
+                final String phone_no = Phone_no.getText().toString().trim();
                 String password = Password.getText().toString().trim();
+
+
+                databaseReference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        userInfo usrinf = dataSnapshot.getValue(userInfo.class);
+                        if(phone_no.equals(usrinf.getPhone_no())){
+                            Toast.makeText(getApplicationContext(),"Phone Already number exists",Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
 
                 //if fields are not empty:
                 if( !name.isEmpty() && !address.isEmpty() && !nid_no.isEmpty() && !phone_no.isEmpty() && !password.isEmpty() ) {
@@ -55,6 +76,22 @@ public class _4Register extends AppCompatActivity {
 
                     Intent intent1 = new Intent(_4Register.this, _3Login.class);
                     startActivity(intent1);
+                }
+
+                //else fields are empty
+                else{
+                    Toast.makeText(getApplicationContext(),"Empty Input Field",Toast.LENGTH_SHORT).show();
+
+                    if(name.isEmpty())
+                        Name.setError("Input your name.");
+                    else if(address.isEmpty())
+                        Address.setError("Input your address");
+                    else if(nid_no.isEmpty())
+                        Nid_no.setError("Input your National ID Number");
+                    else if(password.isEmpty())
+                        Password.setError("Input a password");
+                    else if(phone_no.isEmpty())
+                        Phone_no.setError("Input your address");
                 }
 
 
@@ -78,7 +115,7 @@ public class _4Register extends AppCompatActivity {
         databaseReference.child(key).setValue(usrinf);
 
         //toast for showing registration done message
-        Toast.makeText(getApplicationContext(),"Registration Done",Toast.LENGTH_LONG).show();
+        //Toast.makeText(getApplicationContext(),"Registration Done",Toast.LENGTH_LONG).show();
 
     }
 }
